cbb8dca6a0cc17851d85eefe112936911cd86b97
gendloop <gendloop@163.com>
2023-06-16 19:04:44 +0800

    chore: update

--- docs/replace.pl
+++ docs/replace.pl
@@ -11,9 +11,9 @@ my @content = do { local $/; <$fh_in> };
 close $fh_in;
 
 foreach my $index (6, 24) {
-  my $content[$index] =~ s/\Q$old_string\E/$new_string/g;
+  @content[$index] =~ s/\Q$old_string\E/$new_string/g;
 }
 
 open my $fh_out, '>', $output_file or die "Cannot open output file: $!";
-print $fh_out $content;
+print $fh_out @content;
 close $fh_out;
