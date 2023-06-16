15c7df45884b8ea0c8912280bda8544ee01b983e
gendloop <gendloop@163.com>
2023-06-16 17:59:10 +0800

    chore: update

--- docs/replace.pl
+++ docs/replace.pl
@@ -3,12 +3,14 @@ use warnings;
 
 my $input_file = shift @ARGV;
 my $output_file = shift @ARGV;
+my $old_string = shift @ARGV;
+my $new_string = shift @ARGV;
 
 open my $fh_in, '<', $input_file or die "Cannot open input file: $!";
 my $content = do { local $/; <$fh_in> };
 close $fh_in;
 
-$content =~ s/NewProject/OldProject/g;
+$content =~ s/\Q$old_string\E/$new_string/g;
 
 open my $fh_out, '>', $output_file or die "Cannot open output file: $!";
 print $fh_out $content;
