81b25df89e82eab29a9585b86096caa37a5f09a7
gendloop <gendloop@163.com>
2023-06-16 19:39:23 +0800

    chore: update

--- docs/replace.pl
+++ docs/replace.pl
@@ -5,9 +5,10 @@ my $input_file = shift @ARGV;
 my $output_file = shift @ARGV;
 my $old_string = shift @ARGV;
 my $new_string = shift @ARGV;
+my $content
 
 open my $fh_in, '<', $input_file or die "Cannot open input file: $!";
-my $content = do { local $/; <$fh_in> };
+$content = do { local $/; <$fh_in> };
 close $fh_in;
 
 $content =~ s/$old_string/$new_string/g;
